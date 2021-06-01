b5b4c20974e1db3757dffee201a2ab152e9f9cbd
tuanta911 <tuan.thaianh92@gmail.com>
2021-06-01 22:14:07 +0700

    add product detail guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { ProductDetailGuard } from './product/product-detail/product-detail.guard';
 import { WelcomeComponent } from './home/welcome.component';
 import { ProductDetailComponent } from './product/product-detail/product-detail.component';
 import { ProductListComponent } from './product/product-list/product-list.component';
@@ -6,7 +7,11 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: 'products', component: ProductListComponent },
-  { path: 'products/:id', component: ProductDetailComponent },
+  {
+    path: 'products/:id',
+    canActivate: [ProductDetailGuard],
+    component: ProductDetailComponent,
+  },
   { path: 'welcome', component: WelcomeComponent },
   { path: '', redirectTo: 'welcome', pathMatch: 'full' },
   { path: '**', component: WelcomeComponent },
